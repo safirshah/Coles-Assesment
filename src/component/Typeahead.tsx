aabb41300c01aaf7a3d6d6c58aa40db880c5fb7c
Muhammed Safir <safir.safir2010@gmail.com>
2024-08-06 16:39:29 +0530

    Added suggestion changes

--- src/component/Typeahead.tsx
+++ src/component/Typeahead.tsx
@@ -15,6 +15,7 @@ const Typeahead: React.FC = () => {
   const [input, setInput] = useState('');
   const items: ItemType[] = data.items;
   const [filteredData, setFilteredData] = useState<ItemType[]>(items);
+  const [suggestions, setSuggestions] = useState<ItemType[]>([]);
   
   // Function to handle input change
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,6 +27,22 @@ const Typeahead: React.FC = () => {
       (item.name.toLowerCase().includes(value.toLowerCase()) || item.category.toLowerCase().includes(value.toLowerCase()))
     );
     setFilteredData(filteredItems);
+
+    // Update suggestions based on filtered items
+    if (value.length > 0) {
+      const matches = filteredItems
+      setSuggestions(matches);
+    } else {
+      setSuggestions([]);
+    }
+
+  };
+
+  // Function to handle suggestion click
+  const handleSuggestionClick = (item: ItemType) => {
+    setInput(item.name); 
+    setFilteredData([item]); 
+    setSuggestions([]); 
   };
 
   return (
@@ -37,6 +54,15 @@ const Typeahead: React.FC = () => {
         onChange={handleInputChange}
         placeholder="Search with category or item (For eg: Fruits, Electronics, Apple)"
       />
+      {suggestions.length > 0 && (
+        <div className="card suggestions">
+          {suggestions.map(item => (
+            <div className="slist" key={item.id} onClick={() => handleSuggestionClick(item)}>
+              {item.name}
+            </div>
+          ))}
+        </div>
+      )}
       <Homepage data={filteredData} />
     </div>
   );
