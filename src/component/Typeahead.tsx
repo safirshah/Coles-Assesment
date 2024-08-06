b033dfcf7f959cf4b545e87a8e6978f495ad6819
Muhammed Safir <safir.safir2010@gmail.com>
2024-08-06 17:13:40 +0530

    Added suggestion visibility changes

--- src/component/Typeahead.tsx
+++ src/component/Typeahead.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import Homepage from './HomePage'
+import Homepage from './HomePage';
 import data from '../fakeApi.json';
-;
+
 // Interface for the item in the list
 interface ItemType {
   id: number;
@@ -16,7 +16,8 @@ const Typeahead: React.FC = () => {
   const items: ItemType[] = data.items;
   const [filteredData, setFilteredData] = useState<ItemType[]>(items);
   const [suggestions, setSuggestions] = useState<ItemType[]>([]);
-  
+  const [isFocused, setIsFocused] = useState(false); // State to track input focus
+
   // Function to handle input change
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -30,19 +31,35 @@ const Typeahead: React.FC = () => {
 
     // Update suggestions based on filtered items
     if (value.length > 0) {
-      const matches = filteredItems
+      const matches = filteredItems;
       setSuggestions(matches);
     } else {
       setSuggestions([]);
     }
-
   };
 
   // Function to handle suggestion click
   const handleSuggestionClick = (item: ItemType) => {
-    setInput(item.name); 
-    setFilteredData([item]); 
-    setSuggestions([]); 
+    setInput(item.name);
+    setFilteredData([item]);
+    setSuggestions([]);
+  };
+
+  // Function to handle input blur
+  const handleInputBlur = () => {
+    setIsFocused(false);
+    // Hide suggestions when input loses focus
+    setSuggestions([]);
+  };
+
+  // Function to handle input focus
+  const handleInputFocus = () => {
+    setIsFocused(true);
+    // Re-filter suggestions based on current input value
+    const filteredItems = items.filter(item =>
+      (item.name.toLowerCase().includes(input.toLowerCase()) || item.category.toLowerCase().includes(input.toLowerCase()))
+    );
+    setSuggestions(filteredItems);
   };
 
   return (
@@ -52,9 +69,11 @@ const Typeahead: React.FC = () => {
         value={input}
         className='sbox'
         onChange={handleInputChange}
+        onBlur={handleInputBlur}
+        onFocus={handleInputFocus}
         placeholder="Search with category or item (For eg: Fruits, Electronics, Apple)"
       />
-      {suggestions.length > 0 && (
+      {isFocused && suggestions.length > 0 && (
         <div className="card suggestions">
           {suggestions.map(item => (
             <div className="slist" key={item.id} onClick={() => handleSuggestionClick(item)}>
